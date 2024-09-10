ddd9044763d7f33de90decfd8f0d29bb7b1a9e44
Kaoru <kaoru@gmail.com>
2024-09-10 15:43:18 +0800

    完成註冊

--- user/views.py
+++ user/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
+# 註冊
+def user_register(request):
+    message = ""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            message = "註冊成功"
+
+        else:
+            message = "資料錯誤:"
+
+    else:
+        form = UserCreationForm()
+
+    return render(
+        request, "user/user-register.html", {"message": message, "form": form}
+    )
