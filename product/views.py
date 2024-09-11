dc8c40159dea6f6dbd977590319312a0d4bae40d
Kaoru <kaoru@gmail.com>
2024-09-11 23:35:10 +0800

    review-product-detail邏輯註解

--- product/views.py
+++ product/views.py
@@ -4,6 +4,7 @@ from .forms import ProductForm
 from django.core.paginator import Paginator
 from django.contrib.admin.views.decorators import staff_member_required
 from cart.forms import CartItemForm
+from cart.models import CartItem
 
 
 # Create your views here.
@@ -59,6 +60,7 @@ def review_product(request):
 # 瀏覽商品內容&加入購物車
 def review_product_detail(request, product_id):
     message = ""
+    # 抓取該商品資訊
     try:
         product = Product.objects.get(id=product_id)
 
@@ -66,20 +68,31 @@ def review_product_detail(request, product_id):
         return redirect("review-product")
 
     if request.method == "POST":
+        # 只有數量可以填
         form = CartItemForm(request.POST)
 
         if form.is_valid():
-            # 抓取欄位
+            # 抓取欄數量欄位做庫存比較
             quantity = form.cleaned_data.get("quantity")
 
             if product.product_stock < quantity:
                 message = f"庫存不足，最大庫存量為{product.product_stock}"
+
             else:
-                cartitemform = form.save(commit=False)
-                cartitemform.product = product
-                cartitemform.user = request.user
-                cartitemform.save()
-                message = "商品已成功加入購物車"
+                try:
+                    cartitem = CartItem.objects.get(user=request.user, product=product)
+                    # 如果商品已經在購物車中，增加數量
+                    cartitem.quantity += quantity
+                    cartitem.save()
+                    message = "已有同項商品了，幫你累加數量"
+
+                # 如果商品不在購物車中，新增cartitem實體物件
+                except CartItem.DoesNotExist:
+                    cartitemform = form.save(commit=False)
+                    cartitemform.product = product
+                    cartitemform.user = request.user
+                    cartitemform.save()
+                    message = "商品已成功加入購物車"
 
                 # 更新庫存(因product為某一商品的實體物件，擁有該實體物件的所有內容，並可以做運算)
                 product.product_stock -= quantity
