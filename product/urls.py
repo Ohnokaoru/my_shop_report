e0670a36ca77f37a15a9fdba7d17dd4792a48973
Kaoru <kaoru@gmail.com>
2024-09-11 12:52:50 +0800

    1.新增review-product(沒debug) 2.將review-product分為staff與一般使用者

--- product/urls.py
+++ product/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("create-product/", views.create_product, name="create-product"),
+    path("review-product/", views.review_product, name="review-product"),
+    path("", views.review_product, name="review-product"),
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from .forms import ProductForm
+from django.core.paginator import Paginator
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 # Create your views here.
@@ -13,8 +15,10 @@ def create_product(request):
         if form.is_valid():
             productform = form.save(commit=False)
             productform.sales_quantity = 0
+
+            print(f"productform.product_img.path")
             productform.save()
-            message = "新增成功"
+            return redirect("review-product")
 
         else:
             message = "資料錯誤"
@@ -25,3 +29,26 @@ def create_product(request):
     return render(
         request, "product/create-product.html", {"form": form, "message": message}
     )
+
+
+# 瀏覽商品
+def review_product(request):
+    produsts = Product.objects.all().order_by("id")
+    if not produsts:
+        return redirect("create-product")
+
+    paginator = Paginator(produsts, 4)
+    try:
+        page_number = int(request.GET.get("page", 1))
+
+    except (ValueError, TypeError):
+        page_number = 1
+
+    page_obj = paginator.get_page(page_number)
+
+    if request.user.is_staff:
+        templates = "product/review-product-staff.html"
+    else:
+        templates = "product/review-product.html"
+
+    return render(request, templates, {"page_obj": page_obj})
