e3bc752168be349a777697964a9f765e46e24ea4
Kaoru <kaoru@gmail.com>
2024-09-12 16:29:16 +0800

    新增order models form

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+
 # Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "order_time", "shipping_address", "status")
+    list_filter = ("id", "status")
+    ordering = "-order_time"
+
+
+class OrderItem(admin.ModelAdmin):
+    list_display = ("id", "product_name", "quantity", "order_price")
+    ordering = ("-id",)
--- order/models.py
+++ order/models.py
@@ -15,7 +15,7 @@ class Order(models.Model):
         return f"{self.user.username} {self.shipping_address}"
 
 
-# 轉移購物車內容用
+# 轉移購物車內容到此，商品詳細內容
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -23,4 +23,4 @@ class OrderItem(models.Model):
     order_price = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return f"商品:{self.product.product_name} 數量:{self.quantity} 下單時商品價格:{self.order_price}"
+        return f"商品:{self.product.product_name} 數量:{self.quantity} 下單時商品價格:{self.order_price} "
