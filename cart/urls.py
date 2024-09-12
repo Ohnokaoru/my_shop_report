517744d28cd3f347023eb278e79fd5aa44f7c2c3
Kaoru <kaoru@gmail.com>
2024-09-12 11:33:23 +0800

    修改review-cart介面

--- cart/urls.py
+++ cart/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("review-cart/", views.review_cart, name="review-cart"),
+    path("edit-cart/<int:product_id>", views.edit_cart, name="edit-cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
-# 檢視購物車
+# # 檢視購物車
 @login_required
 def review_cart(request):
     total_amount = 0
@@ -28,4 +28,31 @@ def review_cart(request):
         request,
         "cart/review-cart.html",
         {"cartitems": cartitems, "total_amount": total_amount},
+    )
+
+
+# # 修改購物車
+@login_required
+def edit_cart(request, product_id):
+    message = ""
+
+    cartitem = CartItem.objects.get(user=request.user, product__id=product_id)
+
+    if request.method == "POST":
+        form = CartItemForm(request.POST, instance=cartitem)
+
+        if form.is_valid():
+            quantity = form.cleaned_data.get("quantity")
+            if quantity > cartitem.product.product_stock:
+                message = f"數量大於庫存，目前庫存量為:{cartitem.product.product_stock}"
+
+            form.save()
+            message = "更新成功"
+    else:
+        form = CartItemForm(instance=cartitem)
+
+    return render(
+        request,
+        "cart/edit-cart.html",
+        {"cartitems": cartitem, "form": form, "message": message},
     )
